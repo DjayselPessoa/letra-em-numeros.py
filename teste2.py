791e090afe539e81c2edcae0f0a153ef293c562d
Djaysel Pessôa <djaysel@outlook.com>
2021-08-09 22:37:20 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -1,54 +1,55 @@
 from folder1.funcaoExt1 import calculo
-strIterar = str(input("INFORME SEU NOME SEM ACENTO E COM LETRA MINÚSCULA: "))
+iT = str(input("INFORME SEU NOME: "))
+iT = iT.lower()
 valor = 0
 valorN = 0
-indice = 0
+i = 0
 
 
-while indice < len(strIterar):
-    
-    if strIterar[indice] == "a" or strIterar[indice] == "j" or strIterar[indice] == "s":
-        valor = valor + 1
-        print (strIterar[indice], valor)
-    elif strIterar[indice] == "b" or strIterar[indice] == "k" or strIterar[indice] == "t":
-        valor = valor + 2
-        print (strIterar[indice], valor)
-    elif strIterar[indice] == "c" or strIterar[indice] == "ç" or strIterar[indice] == "l" or strIterar[indice] == "u":
-        valor = valor + 3
-        print (strIterar[indice], valor)
-    elif strIterar[indice] == "d" or strIterar[indice] == "m" or strIterar[indice] == "v":
-        valor = valor + 4
-        print (strIterar[indice], valor)
-    elif strIterar[indice] == "e" or strIterar[indice] == "n" or strIterar[indice] == "w":
-        valor = valor + 5
-        print (strIterar[indice], valor)
-    elif strIterar[indice] == "f" or strIterar[indice] == "o" or strIterar[indice] == "x":
-        valor = valor + 6
-        print (strIterar[indice], valor)
-    elif strIterar[indice] == "g" or strIterar[indice] == "p" or strIterar[indice] == "y":
-        valor = valor + 7
-        print (strIterar[indice], valor)
-    elif strIterar[indice] == "h" or strIterar[indice] == "q" or strIterar[indice] == "z":
-        valor = valor + 8
-        print (strIterar[indice], valor)
-    elif strIterar[indice] == "i" or strIterar[indice] == "r":
-        valor = valor + 9
-        print (strIterar[indice], valor)
-    elif strIterar[indice] == " ":
-        valorN = valorN + valor
-        print (valorN)
-        valor = 0
-    
-    indice += 1
+while i < len(iT):
+    try:
+        if iT[i] == "a" or iT[i] == "ã" or iT[i] == "j" or iT[i] == "s":
+            valor = valor + 1
+            print(iT[i], valor)
+        elif iT[i] == "b" or iT[i] == "k" or iT[i] == "t":
+            valor = valor + 2
+            print(iT[i], valor)
+        elif iT[i] == "c" or iT[i] == "ç" or iT[i] == "l" or iT[i] == "u":
+            valor = valor + 3
+            print(iT[i], valor)
+        elif iT[i] == "d" or iT[i] == "m" or iT[i] == "v":
+            valor = valor + 4
+            print(iT[i], valor)
+        elif iT[i] == "e" or iT[i] == "é" or iT[i] == "n" or iT[i] == "w":
+            valor = valor + 5
+            print(iT[i], valor)
+        elif iT[i] == "f" or iT[i] == "o" or iT[i] == "õ" or iT[i] == "ô" or iT[i] == "x":
+            valor = valor + 6
+            print(iT[i], valor)
+        elif iT[i] == "g" or iT[i] == "p" or iT[i] == "y":
+            valor = valor + 7
+            print(iT[i], valor)
+        elif iT[i] == "h" or iT[i] == "q" or iT[i] == "z":
+            valor = valor + 8
+            print(iT[i], valor)
+        elif iT[i] == "i" or iT[i] == "r":
+            valor = valor + 9
+            print(iT[i], valor)
+        elif iT[i] == " ":
+            valorN = valorN + valor
+            print(valorN)
+            valor = 0
+        else:
+            raise ValueError(" - caractere especial detectado!")
+        i += 1
+    except ValueError as e:
+        print("Inválido!", e)
+        break
 
-
-if indice == len(strIterar) and strIterar[indice - 1] == " ":
-    print (valorN);
-    print (calculo(valorN));
+if i == len(iT) and iT[i - 1] == " ":
+    print(valorN)
+    print(calculo(valorN))
 else:
     valor2 = valorN + valor
-    print (valorN + valor)
-    print (calculo(valor2))
-
-    
-
+    print(valorN + valor)
+    print(calculo(valor2))
