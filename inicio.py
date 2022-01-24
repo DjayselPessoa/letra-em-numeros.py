d25f3b7054ed6d3d63a81919dfb2559078ba786d
Djaysel Pessôa <djaysel@outlook.com>
2022-01-24 14:24:39 -0300

    update#05

--- inicio.py
+++ inicio.py
@@ -2,48 +2,47 @@ nome = str(input("INFORME SEU NOME: "))
 nome = nome.lower()
 valor = 0
 i = 0
+active = True
 
 while i < len(nome):
-    try:
-        if nome[i] in "aáãjs":
-            valor = valor + 1
-            print(f" {nome[i]}  -> {1} : {valor} ")
-        elif nome[i] in "bkt":
-            valor = valor + 2
-            print(f" {nome[i]}  -> {2} : {valor} ")
-        elif nome[i] in "cçlu":
-            valor = valor + 3
-            print(f" {nome[i]}  -> {3} : {valor} ")
-        elif nome[i] in "dmv":
-            valor = valor + 4
-            print(f" {nome[i]}  -> {4} : {valor} ")
-        elif nome[i] in "eénw":
-            valor = valor + 5
-            print(f" {nome[i]}  -> {5} : {valor} ")
-        elif nome[i] in "foõôx":
-            valor = valor + 6
-            print(f" {nome[i]}  -> {6} : {valor} ")
-        elif nome[i] in "gpy":
-            valor = valor + 7
-            print(f" {nome[i]}  -> {7} : {valor} ")
-        elif nome[i] in "hqz":
-            valor = valor + 8
-            print(f" {nome[i]}  -> {8} : {valor} ")
-        elif nome[i] in "ir":
-            valor = valor + 9
-            print(f" {nome[i]}  -> {9} : {valor} ")
-        elif nome[i] in " ":
-            pass
-        else:
-            raise ValueError(" - caractere especial detectado!\n Reinicie a aplicação")
-        i += 1
-    except ValueError as e:
-        print("Inválido!", e)
+    if nome[i] in "aáãjs":
+        valor = valor + 1
+        print(f" {nome[i]}  -> {1} : {valor} ")
+    elif nome[i] in "bkt":
+        valor = valor + 2
+        print(f" {nome[i]}  -> {2} : {valor} ")
+    elif nome[i] in "cçlu":
+        valor = valor + 3
+        print(f" {nome[i]}  -> {3} : {valor} ")
+    elif nome[i] in "dmv":
+        valor = valor + 4
+        print(f" {nome[i]}  -> {4} : {valor} ")
+    elif nome[i] in "eénw":
+        valor = valor + 5
+        print(f" {nome[i]}  -> {5} : {valor} ")
+    elif nome[i] in "foõôx":
+        valor = valor + 6
+        print(f" {nome[i]}  -> {6} : {valor} ")
+    elif nome[i] in "gpy":
+        valor = valor + 7
+        print(f" {nome[i]}  -> {7} : {valor} ")
+    elif nome[i] in "hqz":
+        valor = valor + 8
+        print(f" {nome[i]}  -> {8} : {valor} ")
+    elif nome[i] in "ir":
+        valor = valor + 9
+        print(f" {nome[i]}  -> {9} : {valor} ")
+    elif nome[i] in " ":
+        pass
+    else:
+        print("- caractere incompatível detectado!")
+        active = False
         break
+    i += 1
 cont1 = 0
 cont2 = 0
 print(f"valor parcial: {valor}")
-active = True
+
 while active:
     print("partes em unidade: ")
     for i in str(valor):
@@ -59,4 +58,7 @@ while active:
         continue
     elif len(str(valor)) == 1:
         break
-print("Valor final é: " + str(valor))
+if active == False:
+    print("Reinicie a aplicação!")
+else:
+    print("Valor final é: " + str(valor))
